d81f42ded7c5214a49f991ed8a5835140fc9e100
Vladimir <vladimirpedenko936@gmail.com>
2022-07-30 17:11:23 +0600

    "реализовал создание, изменение, удаление опросов"

--- Source/Interview/urls.py
+++ Source/Interview/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from Interview.views.poll_views import IndexPollView, DetailPollView
+from Interview.views.poll_views import IndexPollView, DetailPollView, CreatePollView, UpdatePollView, DeletePollView
 
 urlpatterns = [
     path('', IndexPollView.as_view(), name='polls_view'),
     path('polls/<int:pk>/detail/', DetailPollView.as_view(), name='detail_polls_view'),
+    path('polls/create/', CreatePollView.as_view(), name='create_poll'),
+    path('polls/<int:pk>/update/', UpdatePollView.as_view(), name='update_poll'),
+    path('polls/<int:pk>/delete/', DeletePollView.as_view(), name='delete_poll'),
 ]
--- Source/Interview/views/poll_views.py
+++ Source/Interview/views/poll_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.shortcuts import get_object_or_404, reverse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from Interview.forms import PollForm
 from Interview.models import Poll
@@ -23,3 +24,28 @@ class DetailPollView(DetailView):
         context = super().get_context_data(**kwargs)
         context['Polls'] = self.object.For_polls.all()
         return context
+
+
+class CreatePollView(CreateView):
+    template_name = 'Poll/create_poll.html'
+    form_class = PollForm
+
+    def get_success_url(self):
+        return reverse('detail_polls_view', kwargs={'pk': self.object.pk})
+
+
+class UpdatePollView(UpdateView):
+    model = Poll
+    template_name = 'Poll/update_poll.html'
+    form_class = PollForm
+    context_key = 'poll'
+
+    def get_success_url(self):
+        return reverse('detail_polls_view', kwargs={'pk': self.object.pk})
+
+
+class DeletePollView(DeleteView):
+    template_name = 'Poll/delete_poll.html'
+    model = Poll
+    context_object_name = 'poll'
+    success_url = reverse_lazy('polls_view')
