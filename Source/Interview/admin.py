5df5711decca7825ebdbb08afcbabb4f7938c4c4
Vladimir <vladimirpedenko936@gmail.com>
2022-07-30 13:13:39 +0600

    "создал модели 'Poll', 'Choice', зарегистрировал в админке, добавил данные, создал dump.json

--- Source/Interview/admin.py
+++ Source/Interview/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from Interview.models import Poll, Choice
+
+
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['question', 'created_at']
+    list_filter = ['question']
+    search_fields = ['created_at']
+    readonly_fields = ['created_at']
+    fields = ['question', 'created_at']
+
+
+admin.site.register(Poll, PollAdmin)
+
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['option', 'interview']
+    list_filter = ['option']
+    search_fields = ['interview']
+    fields = ['option', 'interview']
+
+
+admin.site.register(Choice, ChoiceAdmin)
--- Source/Interview/models.py
+++ Source/Interview/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import CASCADE
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=150, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+    def __str__(self):
+        return f"{self.id}. {self.question}: {self.created_at}"
+
+    class Meta:
+        db_table = "Poll"
+        verbose_name = "Опрос"
+        verbose_name_plural = "Опросы"
+
+
+class Choice(models.Model):
+    option = models.TextField(max_length=100, verbose_name='Вариант ответа')
+    interview = models.ForeignKey('Interview.Poll', related_name='Poll', on_delete=CASCADE, verbose_name='Опрос')
+
+    def __str__(self):
+        return f"{self.id}. {self.option}: {self.interview}"
+
+    class Meta:
+        db_table = "Choice"
+        verbose_name = "Вариант"
+        verbose_name_plural = "Варианты"
